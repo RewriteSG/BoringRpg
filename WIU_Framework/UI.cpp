7f938b22c59d839e5b5699665c943d72a4d544b5
Halal Hamster <mhazwan112@gmail.com>
2024-08-22 20:02:54 +0800

    Update UI.cpp

--- WIU_Framework/UI.cpp
+++ WIU_Framework/UI.cpp
@@ -127,7 +127,7 @@ void UI::PrintDialogue(Vector2 position, const std::string text) const
 		Sleep(duration);
 		numCh++;
 	}
-	CreateText("[Enter to continue]", Vector2(position.GetX(), position.GetY() + numberOfLine));
+	CreateText("[Enter to continue]", Vector2(position.GetX(), originalPos.GetY() + numberOfLine));
 	while (true)
 	{
 		input = GameManager::_getch();
